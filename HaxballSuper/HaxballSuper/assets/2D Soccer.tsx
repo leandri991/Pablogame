9e65cea94398e25046bfe28a232b2f9ac5d317da
Lucky1x501x3 <lucandinatale@gmail.com>
2024-10-06 18:53:45 -0300

    Delete HaxballSuper/HaxballSuper directory

--- HaxballSuper/HaxballSuper/assets/2D Soccer.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="2D Soccer" tilewidth="8" tileheight="8" tilecount="3600" columns="60">
- <image source="2D Soccer.png" width="480" height="480"/>
-</tileset>
--- HaxballSuper/HaxballSuper/core/src/main/java/com/dinadisotteo/haxballsuper/utiles/Recursos.java
+++
@@ -1,9 +0,0 @@
-package com.dinadisotteo.haxballsuper.utiles;
-
-public class Recursos {
-
-	public static final String LOGO = "fondos/pc.jpg";
-	public static final String FONDO = "fondos/file.png";
-	
-	public static final String FUENTEMENU = "fuentes/Anton.ttf";
-}
